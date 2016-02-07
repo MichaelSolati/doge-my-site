9fd3287b312b2dfed101b522fbc8b82d2fdb6dd1
Michael Solati <mkslt04@gmail.com>
2016-02-07 17:43:47 -0500

    Easing up on parsing

--- textparser.js
+++ textparser.js
@@ -11,8 +11,8 @@ tags.push(document.getElementsByTagName("h3"));
 tags.push(document.getElementsByTagName("h4"));
 tags.push(document.getElementsByTagName("h5"));
 tags.push(document.getElementsByTagName("h6"));
-tags.push(document.getElementsByTagName("li"));
-tags.push(document.getElementsByTagName("a"));
+// tags.push(document.getElementsByTagName("li"));
+// tags.push(document.getElementsByTagName("a"));
 tags.push(document.getElementsByTagName("p"));
 
 var colors = ["red", "green", "blue", "pink", "orange", "yellow"];
@@ -32,28 +32,28 @@ function chooseColor() {
 for (var p = 0; p <= tags.length; p++) {
   try {
     for (var i = 0, len = tags[p].length; i < len; i++) {
-      tag = tags[p][i], text = tag.innerHTML, splitTag = text.split(' ');
-      for (var o = 0; o < splitTag.length; o++) {
-        // Make sure not to replace img tags
-        if (splitTag[o] === "<img") {
-          while (((splitTag[o]).search(/>/i)) === -1){
+      tag = tags[p][i], text = $(tag).text(), splitTag = text.split(' ');
+        for (var o = 0; o < splitTag.length; o++) {
+          // Make sure not to replace img tags
+          if (splitTag[o] === "<img") {
+            while (((splitTag[o]).search(/>/i)) === -1){
+              o++;
+            }
             o++;
-          }
-          o++;
-        } else {
-          if (splitTag.length === 1) {
-            splitTag[o] = chooseModifier() +" "+ chooseWord();
           } else {
-            if (o%2 !== 0) {
-              splitTag[o] = chooseWord();
+            if (splitTag.length === 1) {
+              splitTag[o] = chooseModifier() +" "+ chooseWord();
             } else {
-              splitTag[o] = chooseModifier()
+              if (o%2 !== 0) {
+                splitTag[o] = chooseWord();
+              } else {
+                splitTag[o] = chooseModifier()
+              }
             }
           }
         }
-      }
-      text = splitTag.join(' ');
-      tag.innerHTML = text;
+        text = splitTag.join(' ');
+        $(tag).text(text);
       $(tag).css('color', chooseColor());
     }
   } catch (e) {
