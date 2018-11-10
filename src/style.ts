42d33b6af86909be33fb8e19d794b53eea126509
Michael Solati <mkslt04@gmail.com>
2018-11-09 17:26:05 -0800

    fix(style): fix broken css by using js to inject css (as intended and forgotten)
    Will use to remove css on route change at another time

---
+++ src/style.ts
@@ -0,0 +1,16 @@
+function style() {
+  const styleID = 'doge-my-site-style';
+  const css: string = '* { font-family: "Comic Sans MS", "Comic Sans", cursive !important; }';
+  let style: HTMLStyleElement = document.getElementById(styleID) as HTMLStyleElement;
+  
+  if (style) { document.body.removeChild(style); }
+
+  style = document.createElement('style');
+  style.type = 'text/css';
+  style.setAttribute('id', styleID);
+  style.appendChild(document.createTextNode(css));
+
+  document.body.appendChild(style);
+}
+
+style();
